bec5eccd90512a040c4346099fb806d0455ace6d
Jason Black <jasonanthonblack@gmail.com>
2019-09-05 14:17:51 -0500

    added Community class in Data project

---
+++ BeMyNeighbor.Data/Entities/Community.cs
@@ -0,0 +1,10 @@
+namespace BeMyNeighbor.Data.Entities
+{
+    public class Community
+    {
+      public int Id { get; set; }
+      public string ComName { get; set; }
+      public string Geolocation { get; set; }
+      public string Radius { get; set; }
+    }
+}
