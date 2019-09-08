c7efe8bcb48b703f9c91ff0ebc3468f7fd5b87b6
luis alvarez <alvarez.en.luis@gmail.com>
2019-09-08 12:52:42 -0500

    remove test files

--- testfile.cs
+++
@@ -1,7 +0,0 @@
-namespace Name
-{
-    public class Run{
-      public int speed { get; set; }
-      public Vector velocity { get; set; }
-    }
-}
--- testfile2.cs
+++
@@ -1,7 +0,0 @@
-namespace Name
-{
-  public class Ship{
-    public decimal Weight { get; set; }
-    public int MaxSpeed { get; set; }
-  }   
-}
