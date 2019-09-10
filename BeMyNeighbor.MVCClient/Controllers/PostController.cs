993045ebeb0fb5ec25d1069ad443d16271784395
luis alvarez <alvarez.en.luis@gmail.com>
2019-09-09 21:09:04 -0500

    updated controller due to CreatePostViewModel class

---
+++ BeMyNeighbor.MVCClient/Controllers/PostController.cs
@@ -0,0 +1,33 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using BeMyNeighbor.Models.DbModels;
+
+namespace BeMyNeighbor.MVCClient.Controllers{
+  public class PostController: Controller{
+    public IActionResult Index(){
+      return View();
+    }
+
+    [HttpGet]
+    public IActionResult Create(){
+      return View(new CreatePostViewModel());
+    }
+
+    [HttpPost]
+    public IActionResult Create(CreatePostViewModel model){
+      PostDbManager.CreatePost(model);
+      return RedirectToAction("Index", "Post");
+    }
+
+    public IActionResult CommunityPosts(){
+      ViewBag.listOfPosts = PostDbManager.FetchPostsByCommunityId(2);
+      return View();
+    }
+
+    public IActionResult SelectedPost(int postId){
+      PostDbManager.SelectedPost(postId);
+      return View();
+    }
+
+  }
+}
