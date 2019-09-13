a93bcc0459a6ddaff696af3bb4c851841192cf1f
luis alvarez <alvarez.en.luis@gmail.com>
2019-09-13 13:21:40 -0500

    modified homecontroller [#168486920]

--- BeMyNeighbor.MVCClient/Controllers/HomeController.cs
+++ BeMyNeighbor.MVCClient/Controllers/HomeController.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using BeMyNeighbor.MVCClient.Models;
+using BeMyNeighbor.Domain.Models;
 
 namespace BeMyNeighbor.MVCClient.Controllers
 {
