b28d93979445e0b2b3dfee4334a2a4909ee3013a
luis alvarez <alvarez.en.luis@gmail.com>
2019-09-13 13:27:08 -0500

    modified the currentuser.cs to have a class of localuser for now [#168486970]

--- BeMyNeighbor.Domain/Models/CurrentUser.cs
+++ BeMyNeighbor.Domain/Models/CurrentUser.cs
@@ -1,12 +1,15 @@
+using BeMyNeighbor.Domain.Models;
 using BeMyNeighbor.Data.Entities;
 
 namespace BeMyNeighbor.Domain.Models{
   public class CurrentUser{
-    private static User _user;
+    private static LocalUser _user;
     private CurrentUser(){}
-    public static User Storage(){
+    public static LocalUser Storage(){
       if(_user == null){
-        _user = new User();
+        _user = new LocalUser();
+        _user.UserDb = new Data.Entities.User();
+        _user.Messages = new Messages();
       }
       return _user;
     }
@@ -14,9 +17,19 @@ namespace BeMyNeighbor.Domain.Models{
     //once the user successful login we want to save his 
     //information in a singleton for future use throughout 
     //the application. 
-    public static void SetStorage(User signinUser){
+    public static void SetStorage(LocalUser signinUser){
       _user = signinUser;
     } 
 
+    public Messages Messages { get; set; }
+  }
+
+  public class LocalUser{
+    public User UserDb { get; set; }
+    public Messages Messages { get; set; }
+
+    //SignIn or SignUp Properties
+    public string Password { get; set; }
+    public string UsernameOrEmail { get; set;}
   }
 }
