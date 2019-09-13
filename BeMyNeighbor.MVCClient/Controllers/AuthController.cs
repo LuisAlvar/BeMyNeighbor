3f96d5b29d8e3deca97f08c83e59430e805af903
luis alvarez <alvarez.en.luis@gmail.com>
2019-09-13 13:18:58 -0500

    setup user sign in [#168486840]

--- BeMyNeighbor.MVCClient/Controllers/AuthController.cs
+++ BeMyNeighbor.MVCClient/Controllers/AuthController.cs
@@ -1,7 +1,17 @@
 using Microsoft.AspNetCore.Mvc;
-
+using BeMyNeighbor.Domain.Models;
 namespace BeMyNeighbor.MVCClient.Controllers{
   public class AuthController: Controller{
+    [HttpGet]
+    public ViewResult SignIn(){
+      return View(CurrentUser.Storage());
+    }
 
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public IActionResult SignIn(LocalUser user){
+
+      return RedirectToAction("Index", "Main");
+    }
   }
 }
