8b3e37e81b94b866a86cba512f6d09cea702645f
luis alvarez <alvarez.en.luis@gmail.com>
2019-09-14 11:58:10 -0500

    padding code for UserDbManager.cs [#168495056]

--- BeMyNeighbor.Domain/Models/DbModels/UserDbManager.cs
+++ BeMyNeighbor.Domain/Models/DbModels/UserDbManager.cs
@@ -13,34 +13,33 @@ namespace BeMyNeighbor.Domain.Models.DbModels{
 		}
 		public static UserDbManager GetInstance(){
 			if (instance == null){ 
-      	        instance = new UserDbManager(); 
-    	    } 
-    	return instance; 
-  	    }
-
-        public bool SignUp(Address ads, string firstname, string lastname,
-        string username, string email, string password, int communityId){
-			try{
-                DbManager.GetInstance().User.Add(new User{
-                    Firstname = firstname,
-                    Lastname = lastname,
-                    Address = ads,
-                    Username = username,
-                    HashedPassword = password,
-                    SeedPassword = password,
-                    CommunityId = communityId,
-                    Email = email,
-                    VerifiedUser = false,
-                    NubmerOfPoints = 0
-                });
-                DbManager.GetInstance().SaveChanges();
-				return true;
-			}
-			catch(System.Exception ex){
-                System.Console.WriteLine(ex.ToString());
-				return false;
-			}
-		}
+        instance = new UserDbManager(); 
+    	} 
+      return instance; 
+  	}
+    [Obsolete]
+    public bool SignUp(Address ads, string firstname, string lastname,
+    string username, string email, string password, int communityId){
+      try{
+        DbManager.GetInstance().User.Add(new User{
+            Firstname = firstname,
+            Lastname = lastname,
+            Address = ads,
+            Username = username,
+            HashedPassword = password,
+            SeedPassword = password,
+            CommunityId = communityId,
+            Email = email,
+            VerifiedUser = false,
+            NubmerOfPoints = 0
+        });
+        DbManager.GetInstance().SaveChanges();
+      }catch(System.Exception ex){
+        System.Console.WriteLine(ex.ToString());
+        return false;
+      }
+      return true;
+    }
 
     public bool SignIn(LocalUser user){
       CurrentUser.Storage().Messages.SourceType = "/Auth/SignIn";
