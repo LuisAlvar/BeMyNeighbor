a1fbfce5e76924814a602182bf959bf39395e711
Eslam Ibrahim <ibrahim.eslam@aol.com>
2019-09-15 14:57:13 -0500

    done with post comment and scaffold plus ddl [#168499040]

--- SqlScripts/ddl.sql
+++ SqlScripts/ddl.sql
@@ -102,7 +102,7 @@ create table [Posts].[Post](
   DateSelected DATETIME2 not null,
   UserSelectedID int not null,
 );
-
+alter table [Posts].[Post] add CommentTxt nvarchar (max) not null
 alter table [Posts].[Post]
   add CONSTRAINT Post_PK PRIMARY KEY (PostID);
 
