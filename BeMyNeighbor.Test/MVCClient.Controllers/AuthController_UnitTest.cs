30146a833f9e558477fd1a419783a647c5270ed9
luis alvarez <alvarez.en.luis@gmail.com>
2019-09-16 15:15:49 -0500

    initial auth controller test [#168523892]

---
+++ BeMyNeighbor.Test/MVCClient.Controllers/AuthController_UnitTest.cs
@@ -0,0 +1,9 @@
+using BeMyNeighbor.MVCClient.Controllers;
+using Xunit;
+// using Microsoft.AspNetCore.Mvc;
+
+namespace BeMyNeighbor.Test.MVCClient.Controllers{
+  public class AuthController_UnitTest{
+
+  }
+}
